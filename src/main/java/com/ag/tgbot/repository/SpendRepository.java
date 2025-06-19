6bbbbc5d1b445bded4b06c5ba908012d2e425867
Alexey Gochachilov <agochachilov@gmail.com>
2025-06-19 17:03:06 +0300

    Initial commit

---
+++ src/main/java/com/ag/tgbot/repository/SpendRepository.java
@@ -0,0 +1,9 @@
+package com.ag.tgbot.repository;
+
+import com.ag.tgbot.entity.Spend;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SpendRepository extends JpaRepository<Spend, Long> {
+}
